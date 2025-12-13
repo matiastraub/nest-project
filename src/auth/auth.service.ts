d4d8132e9adfc482bbc8382a1f9e51d3cda7b04e
Matias Traub <matias.traub@gmail.com>
2025-12-13 20:24:06 -0300

    Add authenticate, sigin methods on AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 
 type SignInData = { id: number; email: string };
+type AuthResult = { accessToken: string; id: number; email: string };
 
 @Injectable()
 export class AuthService {
@@ -12,11 +13,28 @@ export class AuthService {
     private readonly jwt: JwtService,
   ) {}
 
+  async authenticate(AuthDto: AuthDto): Promise<AuthResult> {
+    const user = await this.validateUser(AuthDto);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return this.signin(user);
+  }
+
   async validateUser(AuthDto: AuthDto): Promise<SignInData | null> {
     const user = await this.userService.findUserByName(AuthDto.email);
     if (user && user.password == AuthDto.password) {
       return { email: user.email, id: user.id };
     }
     return null;
+  }
+
+  async signin(user: SignInData): Promise<AuthResult> {
+    const tokenPayload = {
+      sub: user.id,
+      username: user.email,
+    };
+    const accessToken = await this.jwt.signAsync(tokenPayload);
+    return { accessToken, email: user.email, id: user.id };
   }
 }
