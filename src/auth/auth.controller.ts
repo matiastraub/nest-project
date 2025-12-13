b53af2e68f3066993c2f93c2324094e5c8668982
Matias Traub <matias.traub@gmail.com>
2025-12-13 20:26:16 -0300

    Add signin method on  AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpStatus,
+  HttpCode,
+  NotImplementedException,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() AuthDto: AuthDto) {
+    if (AuthDto) {
+      return this.authService.authenticate(AuthDto);
+    }
+    throw new NotImplementedException('This method is not implemented');
+  }
+}
