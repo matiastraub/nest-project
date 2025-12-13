a09fe5e2a78837a5c46d572e2fe709227d247ad0
Matias Traub <matias.traub@gmail.com>
2025-12-13 20:14:23 -0300

    Add Jwt Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      //Placeholder secret
+      secret: 'JWT_SECRET',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
 })
 export class AuthModule {}
