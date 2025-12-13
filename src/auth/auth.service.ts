82164c42c3e9b87b81c76649ba23a64a0b4b02ab
Matias Traub <matias.traub@gmail.com>
2025-12-13 20:19:36 -0300

    Add validateUser method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+
+type SignInData = { id: number; email: string };
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwt: JwtService,
+  ) {}
+
+  async validateUser(AuthDto: AuthDto): Promise<SignInData | null> {
+    const user = await this.userService.findUserByName(AuthDto.email);
+    if (user && user.password == AuthDto.password) {
+      return { email: user.email, id: user.id };
+    }
+    return null;
+  }
+}
