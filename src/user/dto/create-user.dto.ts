46c0ad59d19f7850c3901526e12cf0fa4fe80387
Matias Traub <matias.traub@gmail.com>
2025-12-14 21:41:10 -0300

    Update create user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,12 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-  @IsString()
-  name!: string;
+  @IsNotEmpty()
   @IsEmail()
   email!: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  password!: string;
 }
