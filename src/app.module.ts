82bbc9850bd2d8fd06b7bb42d1e1117681fcd6d9
Matias Traub <matias.traub@gmail.com>
2025-12-17 10:57:59 -0300

    Add EventEmitter to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module';
 import { ItemsModule } from './items/items.module';
 import { AccountModule } from './account/account.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TransactionModule } from './transaction/transaction.module';
     ItemsModule,
     AccountModule,
     TransactionModule,
+    EventEmitterModule.forRoot(),
   ],
   controllers: [],
   providers: [],
